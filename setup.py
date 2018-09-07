abe2de4b48e86c8a95bffdcbff16a3ca0637d25b
Wojciech Jakubas <wojciech.jakubas@gmail.com>
2018-09-07 18:31:19 +0200

    Removed stuff not compatible with pycryptodome

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@
 
 from setuptools import setup, find_packages
 
-setup(name='CryptoPlus',
-      version='1.0',
-      description='PyCrypto Cipher extension',
-      author='Christophe Oosterlynck',
+setup(name='pycryptoplus',
+      version='0.0.1',
+      description='pycryptodome (PyCrypto) Cipher extension. Modified to work with pycryptodome.',
+      author='Christophe Oosterlynck. Modified and published by Wojciech Jakubas <wojciech.jakubas@gmail.com>',
       author_email='tiftof@gmail.com',
+      url='https://github.com/voytecPL/pycryptoplus',
       packages = find_packages('src'),
       install_requires = ['pycryptodome'],
       package_dir={'': 'src'}
--- src/CryptoPlus/Util/__init__.py
+++ src/CryptoPlus/Util/__init__.py
@@ -5,11 +5,11 @@ makes the Util modules from Crypto AND CryptoPlus available here
 from __future__ import absolute_import
 #import Crypto
 #from Crypto.Util import number, randpool, RFC1751
-from . import padding, util, python_compat, number, randpool, RFC1751
+from . import padding, util, python_compat, number, RFC1751 #, randpool
 
 from pkg_resources import parse_version
 
-__all__ = ["padding","util","number","randpool","RFC1751","python_compat"]
+__all__ = ["padding","util","number","RFC1751","python_compat"] #,"randpool"
 
 #if parse_version(Crypto.__version__) > parse_version("2.0.1"):
 #        from Crypto.Util import python_compat
